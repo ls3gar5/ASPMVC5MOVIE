ecf60e0d6d685a57e0749c0be3c2cfb9615301a3
lsegars <lsegars@FLX-ARROS-L0049>
2021-10-19 10:56:22 -0300

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!"};
+            
+            var customers = new List<Customer>()
+            {
+                new Customer{ Name = "Jose Customers 1"},
+                new Customer{ Name = "Leo Customers 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel); 
+            
+        }
+
+        public  ActionResult Edit (int id)
+        {
+            return Content($"Hello Worldaaa!! {id}");
+        }
+
+        public ActionResult Index(int? pageIndex = 1, string sortBy = "Name")
+        {
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+        }
+
+    }
+}
